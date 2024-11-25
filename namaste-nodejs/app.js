fa0bd7635505628d0e1a498d5a0f99b8c8b5817e
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-11-25 13:12:48 +0530

    lets write code : global && globalThis && window

---
+++ namaste-nodejs/app.js
@@ -0,0 +1,14 @@
+var name = "Namaste Node" 
+
+var a = 10;
+
+console.log(name)
+console.log(a + a)
+
+console.log(global)
+
+console.log(this) // Returns Empty object : It is not equal to global obj  
+
+console.log(globalThis) // Referce to the global object same in browser also
+
+console.log(globalThis === global) // true
