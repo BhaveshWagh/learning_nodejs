9045a99dfc15f4b57ce1e6a967c7f40f1f4cc4ab
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-11-29 16:40:44 +0530

    require() && IIFE

--- namaste-nodejs/abc.js
+++ namaste-nodejs/abc.js
@@ -1,1 +1,13 @@
-console.log("Namaste abc")
+// require("./path")
+// All the code of the module is wrapped inside a function(IIFE)
+
+// IIFE - Immediately Invoked Function Expression : (anonymous function)()
+
+// (function (module, require) {
+// // All code of the module runs inside here.
+//   require("./path");
+//   function sum(a, b) {
+//     return a + b;
+//   }
+//   module.exports = { sum };
+// })();
